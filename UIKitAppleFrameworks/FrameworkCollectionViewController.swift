ca9adc1710434c4652c91a021166bdbabe291275
Güray Gül <guraygul59@gmail.com>
2024-01-03 17:22:10 +0300

    Rows are configured

--- UIKitAppleFrameworks/FrameworkCollectionViewController.swift
+++ UIKitAppleFrameworks/FrameworkCollectionViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class FrameworkCollectionViewController: UICollectionViewController {
+class FrameworkCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     private let reuseIdentifier = "cell"
     
     var frameworks: [Framework] = MockData.frameworks
+    
+    private let sectionInsets = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 20.0, right: 10.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,7 @@ class FrameworkCollectionViewController: UICollectionViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
     }
     
     // MARK: UICollectionViewDataSource
@@ -38,4 +41,27 @@ class FrameworkCollectionViewController: UICollectionViewController {
         
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let itemsPerRow: CGFloat = 3
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    // 3
+    func collectionView(
+        _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return sectionInsets
+        }
+    
+    // 4
+    func collectionView(
+        _ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return sectionInsets.left
+        }
 }
